9e4a08dde420f746690a843785fb988d8cb8deb6
kalvinsev <kalvinsev@gmail.com>
2026-01-13 17:57:54 -0800

    dfs

--- pellets.py
+++ pellets.py
@@ -40,8 +40,13 @@ class PelletGroup(object):
     def __init__(self, pelletfile):
         self.pelletList = []
         self.powerpellets = []
+        self.pelletfile = pelletfile  # Store filename for restoration
         self.createPelletList(pelletfile)
         self.numEaten = 0
+        self.originalPelletList = []  # Store original pellets for DFS mode
+        self.originalPowerPellets = []
+        self.originalNumEaten = 0
+        self.is_dfs_mode = False  # Track if we're in DFS mode
 
     def update(self, dt):
         for powerpellet in self.powerpellets:
@@ -69,3 +74,48 @@ class PelletGroup(object):
     def render(self, screen):
         for pellet in self.pelletList:
             pellet.render(screen)
+    
+    def store_original_pellets(self):
+        """Store the current state before reducing pellets for DFS mode."""
+        # Store references to current lists and state
+        self.originalPelletList = list(self.pelletList)
+        self.originalPowerPellets = list(self.powerpellets)
+        self.originalNumEaten = self.numEaten
+    
+    def reduce_to_one_pellet(self):
+        """Reduce pellets to just one pellet for DFS mode."""
+        if not self.is_dfs_mode:
+            self.store_original_pellets()
+            self.is_dfs_mode = True
+        
+        # Keep only the first regular pellet (prefer regular over power pellet)
+        if self.pelletList:
+            # Find first regular pellet
+            single_pellet = None
+            for pellet in self.pelletList:
+                if pellet.name == PELLET:
+                    single_pellet = pellet
+                    break
+            
+            # If no regular pellet, use first power pellet
+            if single_pellet is None and self.pelletList:
+                single_pellet = self.pelletList[0]
+            
+            if single_pellet:
+                self.pelletList = [single_pellet]
+                # Update powerpellets list - only include if it's a power pellet
+                if single_pellet.name == POWERPELLET:
+                    self.powerpellets = [single_pellet]
+                else:
+                    self.powerpellets = []
+    
+    def restore_original_pellets(self):
+        """Restore the original pellets when switching away from DFS mode."""
+        if self.is_dfs_mode and self.originalPelletList:
+            # Recreate pellets from the original file to ensure clean state
+            self.pelletList = []
+            self.powerpellets = []
+            self.createPelletList(self.pelletfile)
+            # Restore the number eaten (though this might not be perfect if pellets were eaten)
+            self.numEaten = self.originalNumEaten
+            self.is_dfs_mode = False
